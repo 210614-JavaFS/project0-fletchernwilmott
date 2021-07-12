a42eb1133d8db29070819c64b658a46571bb8903
fletchernwilmott <fwilmottct@gmail.com>
2021-07-12 02:28:28 -0400

    lots of changes

---
+++ src/main/java/com/revature/controllers/AccountController.java
@@ -0,0 +1,113 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.Scanner;
+
+
+import com.revature.models.Account;
+import com.revature.services.AccountService;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class AccountController {
+
+	private static AccountService accountService = new AccountService();
+	
+	
+	private static Scanner sc = new Scanner(System.in);
+	
+	public void accountMenu() {
+		System.out.println("what would you like to do with your accounts?\n"
+				+ "1). see all accounts\n"
+				+ "2) see one account\n"
+				+ "3) add a new account\n"
+				+ "4) return to previous menu");
+		
+		String response = sc.nextLine();
+		
+		switch(response){
+			
+			case "1":
+				showAllAccounts();
+				break;
+			case "2":
+				showOneAccount();
+				break;
+			case"3":
+				addAccount();
+				break;
+			default:
+				return;
+		}
+		
+	}
+	
+	
+	private void showAllAccounts() {
+		List<Account> accounts = accountService.getAllAccounts();
+		for(Account a:accounts) {
+			System.out.println(a);
+		}
+	}
+	
+	private void showOneAccount() {
+		System.out.println("What is the username of the account you want to see?");
+		String response = sc.nextLine();
+		Account account = accountService.getAccount(response);
+		
+		if(account != null) {
+			System.out.println(account);
+		}
+		else {
+			System.out.println("Username not found, try again");
+			showOneAccount();
+		}
+		
+	}
+	
+	private void addAccount() {
+		System.out.println("Please input the username under which you want this account created,"
+				+ " this username must be matched with one in our database of people");
+		String username = sc.nextLine();
+		
+		System.out.println("Please input your desired password");
+		String password = sc.nextLine();
+		
+		System.out.println("Tell us whether this account is going to be a customer, employee, or admin account. (C/E/A)");
+		String accessType = sc.nextLine().toUpperCase();
+		
+		accessTypeLoop:
+		while(true) {
+			// confirm the account type char
+			String temp = "";
+			switch (accessType) {
+				case "C":
+					temp = "Customer";
+					break accessTypeLoop;
+				case "E":
+					temp = "Employee";
+					break accessTypeLoop;
+				case "A":
+					temp = "Admin";
+					break accessTypeLoop;
+				default:
+					// if you enter a wrong account type, go back to the top
+					System.out.println("That isn't an acceptabe access type, try again");
+					continue accessTypeLoop;
+			}
+		}
+		
+		Account account = new Account(username, password, accessType);
+		
+		if(accountService.addAccount(account)) {
+			System.out.println("Your account was added to the database");
+		}
+		else {
+			System.out.println("Something went wrong, account not added");
+		}
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/revature/dao/AccountDAOImpl.java
@@ -0,0 +1,119 @@
+package com.revature.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.models.Account;
+import com.revature.utils.ConnectionUtil;
+
+
+public class AccountDAOImpl implements AccountDAO {
+
+	@Override
+	public List<Account> findAllAccounts() {
+		try(Connection conn = ConnectionUtil.getConnection()){
+			String sql = "SELECT * FROM accounts";
+			
+			Statement statement = conn.createStatement();
+			
+			ResultSet result = statement.executeQuery(sql);
+			
+			List<Account> accountList = new ArrayList<>();
+			
+			//Result sets have a cursor similar to Scanners
+			//moves the cursor forward one row, returns true if there is something
+			while(result.next()) {
+				Account account = new Account();
+				account.setUsername(result.getString("username"));
+				account.setPassword(result.getString("password"));
+				account.setAccessType(result.getString("accessType"));
+				account.setBalance(result.getDouble("balance"));
+				accountList.add(account);
+			}
+			
+			return accountList;
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public Account findByUsername(String username) {
+		try(Connection conn = ConnectionUtil.getConnection()){
+			String sql = "SELECT * FROM accounts WHERE username = ?;";
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			//this is where sql injection is checked for
+			statement.setString(1, username);
+			
+			ResultSet result = statement.executeQuery();
+			
+			Account account = new Account();
+			
+			//Result sets have a cursor similar to Scanners
+			//moves the cursor forward one row, returns true if there is something
+			while(result.next()) {
+				account.setUsername(result.getString("username"));
+				account.setPassword(result.getString("password"));
+				account.setAccessType(result.getString("accessType"));
+				account.setBalance(result.getDouble("balance"));
+			}
+			
+			return account;
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean updateAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAccount(Account account) {
+		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			
+			String sql = "INSERT INTO accounts (username, password, accessType, balance)"
+					+ "VALUES(?, ?, ?, ?);";
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			int index = 0;
+			
+			statement.setString(++index, account.getUsername());
+			statement.setString(++index, account.getPassword());
+			statement.setString(++index, account.getAccessType());
+			statement.setDouble(++index, account.getBalance());
+			
+			statement.execute();
+			
+			return true;
+				
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	
+	
+	
+	
+}
